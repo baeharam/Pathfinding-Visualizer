019cb66c5e4fbc0c15a442f75ff1355ae746bd91
Haram Bae <hisfedev@gmail.com>
2020-01-29 16:52:04 +0900

    :art: Restructure container

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -2,14 +2,23 @@ import React from 'react';
 import Header from '../Header/Header';
 import Board from '../Board/Board';
 import { Provider } from 'Provider';
+import './Container.scss';
 
 const Container = () => {
 
   return (
-    <Provider>
-      <Header />
-      <Board />
-    </Provider>
+    <>
+      <header className="header">
+        <h1 className="header__title">Pathfinding Visualizer</h1>
+      </header>
+      <Provider>
+        <Header />
+        <Board />
+      </Provider>
+      <footer className="footer">
+        <p className="footer__author">Made by Haram Bae</p>
+      </footer>
+    </>
   );
 };
 
