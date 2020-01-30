96e98f597e6afc8d58e4de037d9e06774efb90e9
Haram Bae <hisfedev@gmail.com>
2020-01-30 17:29:23 +0900

    :zap: Optimize through event delegation

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,13 +1,12 @@
 import React, { useContext, useState } from 'react';
 import { Context } from 'Provider';
-import { CLICKED_COLOR, INITIAL_COLOR } from 'constants.js';
+import { CLICKED_COLOR, INITIAL_COLOR, KEYS } from 'constants.js';
 import './Board.scss';
 
 const Board = ({ name, ref }) => {
   const context = useContext(Context);
   const { board, setBoard } = context;
   const [clicking, setClicking] = useState(false);
-
   const onMouseDown = () => {
     setClicking(true);
   };
@@ -15,32 +14,42 @@ const Board = ({ name, ref }) => {
     setClicking(false);
   };
 
+  const changeColor = (e) => {
+    if (e.target.className !== 'board__col') return;
+    if (e.target.style.backgroundColor === INITIAL_COLOR) return;
+    const data = e.target.dataset;
+    const ridx = data.ridx*1, cidx = data.cidx*1;
+    const copy = JSON.parse(JSON.stringify(board));
+    copy[ridx][cidx].color = CLICKED_COLOR;
+    setBoard(copy);
+  }
+
+  const onClick = (e) => {
+    changeColor(e);
+  };
+
+  const onMouseMove = (e) => {
+    if (!clicking) return;
+    changeColor(e);
+  };
+
   return (
-    <div className="board" onMouseDown={onMouseDown} onMouseUp={onMouseUp}>
+    <div className="board" onMouseDown={onMouseDown} onMouseUp={onMouseUp} onClick={onClick} onMouseMove={onMouseMove}>
       {board.map((row,ridx) => (
         <div className="board__row" key={ridx} style={{ display: 'flex', justifyContent: 'center'}}>
-          {row.map((col, cidx) => {
-            let onMouseMove = null;
-            if (board[ridx][cidx].color === INITIAL_COLOR && clicking) {
-              onMouseMove = () => {
-                const copy = JSON.parse(JSON.stringify(board));
-                copy[ridx][cidx].color = CLICKED_COLOR;
-                setBoard(copy);
-              };
-            }
-
-            return (
-              <div 
-                className="board__col" 
-                key={2*ridx+cidx}
-                onMouseMove={onMouseMove}
-                style={{
-                  background: board[ridx][cidx].color,
-                  transition: (clicking ? 'none' : 'background-color 0.3s linear')
-                }} 
-              />
+          {row.map((col, cidx) => (
+            <div 
+              className="board__col"
+              key={KEYS[ridx][cidx]}
+              data-ridx={ridx}
+              data-cidx={cidx}
+              style={{
+                background: board[ridx][cidx].color,
+                transition: (clicking ? 'none' : 'background-color 0.3s linear')
+              }} 
+            />
             )
-          })}
+          )}
           <br/>
         </div>
       ))}
